f09c07f157cfa8b5fd555d13ab2fee80bb103b69
Kirill Paltsev <bestestmaiL@gmail.com>
2014-04-12 20:48:22 +0400

    Episode #3 - http://www.youtube.com/watch?v=u-YyCiy50n4

--- src/com/thecherno/rain/Game.java
+++ src/com/thecherno/rain/Game.java
@@ -11,19 +11,29 @@ public class Game implements Runnable{
   public static int scale = 3;
   
   private Thread thread;
+// для метода run
+  private boolean running = false;
   
 //  synchronized - для предотвращения memory consistency errors
   public synchronized void start(){
+    running = true;
 //  запускаем этот класс в новом потоке
     thread = new Thread(this, "Display");
     thread.start();
   }
 // чтобы точно удалить все треды | не уверен, что это проблема - чувак упоминал проблему с апплетами
   public synchronized void stop(){
+    running = false;
     try {
       thread.join();
     } catch (InterruptedException e) {
       e.printStackTrace();
+    }
+  }
+  
+  public void run(){
+    while(running){
+      
     }
   }
 }
