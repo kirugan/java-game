59df9c65f499ac7537de201687dea35d5681e699
Kirill Paltsev <bestestmaiL@gmail.com>
2014-04-12 20:45:04 +0400

    Episode #2 - http://www.youtube.com/watch?v=V8q55FGysz4

--- src/com/thecherno/rain/Game.java
+++ src/com/thecherno/rain/Game.java
@@ -1,9 +1,29 @@
 package com.thecherno.rain;
 
-public class Game {
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Game implements Runnable{
   public static int width = 300;
 // вычисление высоты в зависимости от ширины при соотношении сторон 16:9
   public static int height = width / 16 * 9;
 // будет увеличивать масштаб
   public static int scale = 3;
+  
+  private Thread thread;
+  
+//  synchronized - для предотвращения memory consistency errors
+  public synchronized void start(){
+//  запускаем этот класс в новом потоке
+    thread = new Thread(this, "Display");
+    thread.start();
+  }
+// чтобы точно удалить все треды | не уверен, что это проблема - чувак упоминал проблему с апплетами
+  public synchronized void stop(){
+    try {
+      thread.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
 }
