76e6a33a50501587d760079359abfd336e7896ad
Kirill Paltsev <bestestmaiL@gmail.com>
2014-04-12 21:04:31 +0400

    Episode #4 - http://www.youtube.com/watch?v=S_CIHzdpgE8

--- src/com/thecherno/rain/Game.java
+++ src/com/thecherno/rain/Game.java
@@ -1,9 +1,13 @@
 package com.thecherno.rain;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
 
-public class Game implements Runnable{
+public class Game extends Canvas implements Runnable{
+// что-то для сериализации - в принципе можно было пропустить т.к. типа станд. соглашение в Java
+  private static final long serialVersionUID = 1L;
+  
   public static int width = 300;
 // вычисление высоты в зависимости от ширины при соотношении сторон 16:9
   public static int height = width / 16 * 9;
@@ -11,9 +15,16 @@ public class Game implements Runnable{
   public static int scale = 3;
   
   private Thread thread;
+  private JFrame frame;
 // для метода run
   private boolean running = false;
   
+  public Game(){
+    Dimension size = new Dimension(width * scale, height * scale);
+    setPreferredSize(size);
+    
+    frame = new JFrame();
+  }
 //  synchronized - для предотвращения memory consistency errors
   public synchronized void start(){
     running = true;
@@ -33,7 +44,23 @@ public class Game implements Runnable{
   
   public void run(){
     while(running){
-      
+      System.out.println("Running ...");
     }
+  }
+  
+  public static void main(String[] args){
+    Game game = new Game();
+    game.frame.setResizable(false);
+    game.frame.setTitle("Rain");
+//  add добавляет компонент в frame - добавляем Game тк он наследуется от Canvas
+    game.frame.add(game);
+//  установить размер frame`a таким же как и у компонента
+    game.frame.pack();
+    game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//  устанавливаем расположение - отцентрирует наше окно относительно экрана
+    game.frame.setLocationRelativeTo(null);
+    game.frame.setVisible(true);
+    
+    game.start();
   }
 }
